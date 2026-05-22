11e78b1af833ebf298f2cd55760bfe7be34dd3fb
ali-elmuzayan <ali.elmuzayn@gmail.com>
2026-05-22 14:57:12 +0300

    NodeJS: Loading the data when the server is run, by using the Promise

--- 03_nodeJs/nasa-app/server/src/models/planets.model.js
+++ 03_nodeJs/nasa-app/server/src/models/planets.model.js
@@ -9,24 +9,36 @@ function isHabitablePlanet(planet) {
     && planet['koi_prad'] < 1.6;
 }
 
-fs.createReadStream('src/data/kepler_data.csv')
-  .pipe(parse({
-    comment: '#',
-    columns: true,
-  }))
-  .on('data', (data) => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-    }
+
+function loadPlanetsDate() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream('src/data/kepler_data.csv')
+    .pipe(parse({
+      comment: '#',
+      columns: true,
+    }))
+    .on('data', (data) => {
+      if (isHabitablePlanet(data)) {
+        habitablePlanets.push(data);
+      }
+    })
+    .on('error', (err) => {
+      console.log(err);
+      reject(err);
+    })
+    .on('end', () => {
+
+      console.log(`${habitablePlanets.length} habitable planets found!`);
+      resolve();
+    });
   })
-  .on('error', (err) => {
-    console.log(err);
-  })
-  .on('end', () => {
-    console.log(habitablePlanets.map((planet) => {
-      return planet['kepler_name'];
-    }));
-    console.log(`${habitablePlanets.length} habitable planets found!`);
-  });
+
+}
+
+
+export  {
+  loadPlanetsDate,
+  
+};
 
 export default habitablePlanets;
--- 03_nodeJs/nasa-app/server/src/server.js
+++ 03_nodeJs/nasa-app/server/src/server.js
@@ -1,8 +1,11 @@
 import { createApp } from './app.js';
 import http from 'http';
-
+import { loadPlanetsDate } from './models/planets.model.js';
 
 const startServer = async () => {
+    // Loads Planets Data
+    await loadPlanetsDate(); 
+
     const PORT = process.env.PORT || 5000;
     try {
         const app = createApp();
