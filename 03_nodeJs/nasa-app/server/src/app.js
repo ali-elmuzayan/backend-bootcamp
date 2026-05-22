a452ec34e7aec3cf88ccc1d300183ca0cdfa146c
ali-elmuzayan <ali.elmuzayn@gmail.com>
2026-05-22 15:34:24 +0300

    NodeJS: Serving react js frontend for productions

--- 03_nodeJs/nasa-app/server/src/app.js
+++ 03_nodeJs/nasa-app/server/src/app.js
@@ -1,21 +1,26 @@
 import express from 'express';
+import path from 'path';
 import planetsRouter from './routes/planets.router.js';
 import cors from 'cors';
 
 export const createApp = () => {
     const app = express();
-
+    const __dirname = path.resolve();
     // Middlewares
     app.use(cors({
         origin: 'http://localhost:3000',
     }));
     app.use(express.json());
+    app.use(express.static(path.join(__dirname, './public'))); // Serve static files from the public directory
 
 
-    // define routes 
+    // define API routes 
     app.use('/api/v1/planets', planetsRouter);
 
-
+    // Serve the React app for the root route
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, './public', 'index.html'));
+    }); 
 
     return app;
 }
